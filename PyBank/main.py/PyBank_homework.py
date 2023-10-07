b3049c913c00b7b2d22b90c5b6a1bc091062f3bb
parisjones <paris.jones901@gmail.com>
2023-10-07 15:51:54 -0700

    PyBank check

--- PyBank/main.py/PyBank_homework.py
+++ PyBank/main.py/PyBank_homework.py
@@ -1,1 +1,24 @@
-print("hello world") 
+#Modules
+import os
+import csv
+
+# start total months counter
+total_rows= 0
+
+#Set path for file
+csvpath= os.path.join("..", "Resources", "budget_data.csv")
+
+#open CSV using UTF-8 encoding
+with open(csvpath, encoding='UTF-8') as csvfile:
+    csvreader= csv.reader(csvfile, delimiter=",")
+    
+    #skip header row
+    header= next(csvreader, None)
+    
+
+    #loop through data counting total months
+    for row in csvreader:
+        total_rows += 1
+    
+    #print the result
+    print(f"Total number of months in the dataset: {total_rows}") 
